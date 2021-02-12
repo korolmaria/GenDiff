e51b1d21d37fe726a6af3640354efa5e0a0989ea
maria <mkweblife@mail.ru>
2021-02-12 12:30:58 +0300

    Fix: refactor index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,6 @@
 // import fs from 'fs';
 import path from 'path';
-import genDiff from '../src/index.js';
+import genDiff from '../src';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
