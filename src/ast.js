57cab2a36107182bc3845777a1ac828d80a59357
Maria Korol <45860744+korolmaria@users.noreply.github.com>
2021-02-13 13:47:44 +0300

    fix: update func getUnionKeys

--- src/ast.js
+++ src/ast.js
@@ -4,7 +4,7 @@ const getUnionKeys = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const unionKeys = _.union(keys1, keys2);
-  return unionKeys.sort();
+  return unionKeys.concat().sort();
 };
 
 const getDiffs = (data1, data2) => {
